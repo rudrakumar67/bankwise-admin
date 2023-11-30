a0802fdf7d17494a00e8e4711626a81c4c5b6f5a
Achu75 <achuma512002@gmail.com>
2023-11-30 15:17:35 +0530

    login ui change

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,15 +1,13 @@
 import SignInForm from '@/app/components/SignInForm'
 import React from 'react'
-
 const SignInPage = () => {
   return (
     <div className='flex flex-col gap-4'>
-        <h1 className='text-3xl'>
-            Sign In
-        </h1>
-        <SignInForm />
+          <h1 className='text-3xl'>
+            SignIn
+          </h1>
+      <SignInForm />
     </div>
   )
 }
-
 export default SignInPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang='en'>
             <body className={inter.className}>
-                <Navbar />
+                {/* <Navbar /> */}
                 <Provider>
                     {children}
                 </Provider>
