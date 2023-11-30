0984af0c91c7007b5565fa05f64c35f2bdb81781
rudrakumar67 <122020483+rudrakumar67@users.noreply.github.com>
2023-11-30 15:29:44 +0530

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className='w-full'>
-      <h1 className='text-3xl'>Home Page</h1>
+      <h1 className='text-3xl'>Home Page test</h1>
     </div>
   )
 }
